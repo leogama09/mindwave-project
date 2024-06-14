95597ad2fbbcdc14e5a5b96b5e856cf3d99b1704
Leonardo Gama <leogama09@gmail.com>
2024-06-13 21:39:01 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Button from "./components/Button"
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
